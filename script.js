79cc5536d7da82ff15744a130498306bae863f3c
Nightingale-9 <ethannaidoo628@gmail.com>
2025-05-05 13:23:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('open');
+  });
+});
